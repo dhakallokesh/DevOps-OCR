31b47fb85ab0071aff7999ce0cb117a5faf50676
kshitizrimal <kshitizrimal2016@gmail.com>
2023-10-16 10:24:48 +0545

    img_url_post

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import os
 import requests
+import urllib.request
 from datetime import datetime
-from typing import Union
-
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI
 from doctr.models import ocr_predictor
 from doctr.io import DocumentFile
 
@@ -15,28 +14,36 @@ def read_root():
     return "OCR Service"
 
 @app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile):
+async def create_upload_file(imgurl: str):
     # api-endpoint
-    URL = "https://ag-sales-bot.onrender.com/async_gpt/"
+    gptURL = "https://ag-sales-bot.onrender.com/async_gpt/"
     now = datetime.now()     
 
     # dd/mm/YY H:M:S
     dt_string = now.strftime("%d%m%Y%H%M%S")
     print("date and time =", dt_string)
 
-    ext_ = file.filename.split(".")[1]    
-    file_location = dt_string+"."+ext_
+    base_name = os.path.basename(imgurl)
+    file_name_with_extension = base_name.split('?')[0]
+    file_name, file_extension = os.path.splitext(file_name_with_extension)
+    final_filename = file_name + file_extension
+    print(final_filename)
+
+    urllib.request.urlretrieve(imgurl, final_filename)
+
+    # ext_ = file.filename.split(".")[1]    
+    # file_location = dt_string+"."+ext_
     
-    with open(file_location, "wb+") as file_object:
-        file_object.write(file.file.read())
+    # with open(file_location, "wb+") as file_object:
+    #     file_object.write(file.file.read())
     
-    single_img_doc = DocumentFile.from_images(file_location)
+    single_img_doc = DocumentFile.from_images(final_filename)
     result = model(single_img_doc)
     response_text = result.render()
-    os.remove(file_location)
+    os.remove(final_filename)
      
     # sending post request and saving the response as response object
-    r = requests.post(url = URL, json={'ocr_text':response_text})
+    r = requests.post(url = gptURL, json={'ocr_text':response_text})
      
     # extracting data in json format
     data = r.json()
