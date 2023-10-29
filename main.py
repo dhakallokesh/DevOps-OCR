b9ff0fc04b6e3a389a0836531dfb0800c7dbd685
kshitizrimal <kshitizrimal2016@gmail.com>
2023-10-29 12:56:12 +0545

    ocr_text_extract

--- main.py
+++ main.py
@@ -9,44 +9,38 @@ from doctr.io import DocumentFile
 app = FastAPI()
 model = ocr_predictor('db_resnet50', "crnn_vgg16_bn", pretrained=True)
 
-@app.get("/")
-def read_root():
-    return "OCR Service"
-
-@app.post("/uploadfile/")
-async def create_upload_file(imgurl: str):
-    print("Input: ", imgurl)
-    # api-endpoint
-    gptURL = "http://172.16.16.54:8080/async_gpt/"
-    now = datetime.now()     
-
-    # dd/mm/YY H:M:S
-    dt_string = now.strftime("%d%m%Y%H%M%S")
-    # print("date and time =", dt_string)
-
+def ocr_run(imgurl: str):
     base_name = os.path.basename(imgurl)
     file_name_with_extension = base_name.split('?')[0]
     file_name, file_extension = os.path.splitext(file_name_with_extension)
     final_filename = file_name + file_extension
-    # print(final_filename)
 
     urllib.request.urlretrieve(imgurl, final_filename)
 
-    # ext_ = file.filename.split(".")[1]    
-    # file_location = dt_string+"."+ext_
-    
-    # with open(file_location, "wb+") as file_object:
-    #     file_object.write(file.file.read())
-    
     single_img_doc = DocumentFile.from_images(final_filename)
     result = model(single_img_doc)
     response_text = result.render()
     os.remove(final_filename)
-     
-    # sending post request and saving the response as response object
+
+    return response_text
+
+@app.get("/")
+def read_root():
+    return "OCR Service"
+
+@app.post("/extract-text/")
+async def extract_text(imgurl: str):
+    response_text = ocr_run(imgurl)
+    return response_text
+
+@app.post("/uploadfile/")
+async def create_upload_file(imgurl: str):
+    print("Input: ", imgurl)
+    gptURL = "http://172.16.16.54:8080/async_gpt/"
+    
+    response_text = ocr_run(imgurl)
     r = requests.post(url = gptURL, json={'ocr_text':response_text})
      
-    # extracting data in json format
     data = r.json()
     print("Output: ", data)
 
